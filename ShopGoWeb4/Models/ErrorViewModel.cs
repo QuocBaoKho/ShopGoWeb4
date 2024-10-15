1f0f147d8501c63d963828d610a702cee2e655f7
QuocBaoKho <baonhq2603@gmail.com>
2024-10-15 14:16:57 +0700

    Add project files.

---
+++ ShopGoWeb4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopGoWeb4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
