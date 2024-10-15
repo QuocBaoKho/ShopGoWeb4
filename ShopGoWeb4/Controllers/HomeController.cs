e43a489877ad603249861c813743f955a0796fdc
QuocBaoKho <baonhq2603@gmail.com>
2024-10-15 15:37:47 +0700

    Put data into web

--- ShopGoWeb4/Controllers/HomeController.cs
+++ ShopGoWeb4/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ShopGoWeb4.Models;
 using System.Diagnostics;
+using X.PagedList;
 
 namespace ShopGoWeb4.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private QlbanVaLiContext _lbanVaLiContext;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _lbanVaLiContext = new QlbanVaLiContext();
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            return View();
+            int pageSize = 8;
+            int pageNum = page==null||page < 0? 1 : page.Value;
+            var ds = _lbanVaLiContext.TDanhMucSps.AsNoTracking().OrderBy(d=>d.TenSp);
+            PagedList<TDanhMucSp> list = new PagedList<TDanhMucSp>(ds, pageNum, pageSize);
+            return View(list);
         }
 
         public IActionResult Privacy()
